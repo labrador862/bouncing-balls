6284e61c21d548ba6888046a4f19d8108a461abe
labrador862 <christopherdehaven02@gmail.com>
2025-01-24 01:47:03 -0500

    added icons and created an executable

--- bounce.py
+++ bounce.py
@@ -1,14 +1,24 @@
 import tkinter as tk
 import random
+import os
+import sys
 
-# initialize window and add title
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+# initialize window, set title and icon
 window = tk.Tk()
 window.title("awesome bouncing")
+window.iconbitmap(resource_path("myicon.ico"))
 
 # size variables, change to whatever
 width = 1000
 height = 500
-numBalls = 25
+numBalls = 100
 msBetweenFrames = 15
 
 # creates place for balls to bounce within the window
@@ -17,19 +27,18 @@ canvas['bg'] = "black"
 canvas.pack(fill=tk.BOTH, expand=1)
 
 # image file (.png) to use as balls 
-ball_image = tk.PhotoImage(file="awesomeface2.png")
+ball_image_path = resource_path("awesomeface2.png")
+ball_image = tk.PhotoImage(file=ball_image_path)
 image_width = ball_image.width()    # gets width and height of used image
 image_height = ball_image.height()  # to improve bounce mechanics
 
 class Ball:
-    def __init__(self, canvas, x, y, vspd, hspd, image, image_width, image_height):
+    def __init__(self, canvas, x, y, vspd, hspd, image):
         self.x = x          # x coordinate
         self.y = y          # y coordinate
         self.hspd = hspd    # horizontal speed
         self.vspd = vspd    # vertical speed
         self.image = image  # .png to represent a ball
-        self.image_width = image_width   # width of png
-        self.image_height = image_height # height of png
         
         self.image_instance = canvas.create_image(self.x, self.y, image = ball_image)
     
@@ -41,9 +50,9 @@ class Ball:
         
         # if the ball hits the edge of the window,
         # reverse its horizontal or vertical direction
-        if(self.x < 0 + self.image_width / 2 or self.x > width - self.image_width / 2): 
+        if(self.x < 0 + image_width / 2 or self.x > width - image_width / 2): 
             self.hspd *= -1
-        if(self.y < 0 + self.image_height / 2 or self.y > height - self.image_height / 2):
+        if(self.y < 0 + image_height / 2 or self.y > height - image_height / 2):
             self.vspd *= -1 
 
 # make space for some balls lol 
@@ -55,8 +64,7 @@ for i in range(0, numBalls):
     y = random.uniform(image_height, height - image_height) # ^^
     vspd = random.uniform(-5, 5)                            # randomly chooses initial speed and direction
     hspd = random.uniform(-5, 5)                            # ^^
-    balls.append(Ball(canvas, x, y, vspd, hspd,             # adds ball to list of balls
-                      ball_image, image_width, image_height))
+    balls.append(Ball(canvas, x, y, vspd, hspd, ball_image))# adds ball to list of balls
 
 # place the ball in the canvas
 def draw():
